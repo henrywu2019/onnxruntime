db1cc75a64471e2ea925ec0eefac74da48d3eef9
Fuheng Wu <fuheng.wu@oracle.com>
2023-01-04 20:56:09 +0000

    update version

--- gme.sh
+++ gme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-./build.sh --config Release --enable_pybind --build_wheel --parallel --skip_tests --skip_onnx_tests --use_dnnl --use_tvm --use_cloud
+./build.sh --config Release --enable_pybind --build_wheel --parallel --skip_tests --skip_onnx_tests --use_dnnl
+#./build.sh --config Release --enable_pybind --build_wheel --parallel --skip_tests --skip_onnx_tests --use_dnnl --use_tvm --use_cloud
 
 
