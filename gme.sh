4e9826e57a3db587ad887eca4462fb77ce2525b1
Fuheng Wu <fuheng.wu@oracle.com>
2023-01-08 19:35:50 +0000

    JEMALLOC

--- gme.sh
+++ gme.sh
@@ -5,4 +5,7 @@ set -x
 #./build.sh --config Release --enable_pybind --build_wheel --parallel --skip_tests --skip_onnx_tests --use_dnnl
 ./build.sh --config Release --enable_pybind --build_wheel --parallel --skip_tests --skip_onnx_tests # --use_dnnl --use_tvm --use_cloud
 
+#--cmake_extra_defines onnxruntime_USE_JEMALLOC=ON  --cmake_extra_defines onnxruntime_USE_AVX2=ON
+
+
 
