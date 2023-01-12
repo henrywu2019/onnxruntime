5ebb9a51902b11aa342dd2db0af3841adfdc0794
HenryFuhengWu <wufuheng@gmail.com>
2023-01-12 09:20:02 -0800

    update eigen to the latest version

--- cmake/external/eigen.cmake
+++ cmake/external/eigen.cmake
@@ -4,16 +4,16 @@ if (onnxruntime_USE_PREINSTALLED_EIGEN)
     file(TO_CMAKE_PATH ${eigen_SOURCE_PATH} eigen_INCLUDE_DIRS)
     target_include_directories(eigen INTERFACE ${eigen_INCLUDE_DIRS})
 else ()
-    if (onnxruntime_USE_ACL)
+    if (onnxruntime_USE_ACL) # Enable for ARM
         FetchContent_Declare(
         eigen
-        URL https://gitlab.com/libeigen/eigen/-/archive/d10b27fe37736d2944630ecd7557cefa95cf87c9/eigen-d10b27fe37736d2944630ecd7557cefa95cf87c9.zip
+        URL https://gitlab.com/libeigen/eigen/-/archive/4d05765345e7e4a984d600039f797e2fede924f3/eigen-4d05765345e7e4a984d600039f797e2fede924f3.zip
         PATCH_COMMAND ${Patch_EXECUTABLE} --ignore-space-change --ignore-whitespace < ${PROJECT_SOURCE_DIR}/patches/eigen/Fix_Eigen_Build_Break.patch
 		)
     else()
         FetchContent_Declare(
         eigen
-        URL https://gitlab.com/libeigen/eigen/-/archive/d10b27fe37736d2944630ecd7557cefa95cf87c9/eigen-d10b27fe37736d2944630ecd7557cefa95cf87c9.zip
+        URL https://gitlab.com/libeigen/eigen/-/archive/4d05765345e7e4a984d600039f797e2fede924f3/eigen-4d05765345e7e4a984d600039f797e2fede924f3.zip
 		)
     endif()
     FetchContent_Populate(eigen)
