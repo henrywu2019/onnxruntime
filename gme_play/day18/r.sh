bc89f2a5fcfb7e92eba9cb8c978e371990509a74
Oracle Public Cloud User <opc@gamma-s28.visionprivatesu.ocasdev01iad.oraclevcn.com>
2023-02-22 05:13:50 +0000

    r

--- gme_play/day18/r.sh
+++ gme_play/day18/r.sh
@@ -2,7 +2,10 @@
 
 set -x
 
-g++ test_sse4.cpp -o test_sse4.exe -I/home/henry/wendy/git.repo/onnxruntime/gme_play/gamma/include \
--L/home/henry/wendy/git.repo/onnxruntime/build/Linux/Release \
- -L/home/henry/wendy/git.repo/onnxruntime/build/Linux/Release/_deps/google_nsync-build \
-  -lgamma_shared -lonnxruntime_mlas -lonnxruntime_common -lnsync_cpp -std=c++20 -O3 -mavx2 -march=native
+ROOT=/home/henry/wendy/git.repo/onnxruntime
+ROOT=~/workspace/onnxruntime
+
+g++ test_sse4.cpp -o test_sse4.exe -I${ROOT}/gme_play/gamma/include \
+-L$ROOT/build/Linux/Release \
+ -L$ROOT/build/Linux/Release/_deps/google_nsync-build \
+  -lgamma -lonnxruntime_mlas -lonnxruntime_common -lnsync_cpp -std=c++20 -O3 -mavx2 -march=native
