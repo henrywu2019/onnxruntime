20db4437f57a7c43d17d7c0dca5b43a5c49e62b1
Fuheng Wu <fuheng.wu@oracle.com>
2023-04-12 15:35:29 +0000

    bug fix

--- onnxruntime/core/oracle/gamma_tn.asm
+++ onnxruntime/core/oracle/gamma_tn.asm
@@ -4,6 +4,9 @@ extern _gamma_hc
 global _gamma_tn
 section .text
 _gamma_tn:
+    push rbx
+    push rcx
+    push rdx
     call _gamma_q
     cmp rax, -1
     je gamma_tn.fall_back
@@ -13,7 +16,13 @@ _gamma_tn:
     je gamma_tn.fall_back
     idiv rbx
     mov rax, rbx
+    pop rdx
+    pop rcx
+    pop rbx
     ret
 gamma_tn.fall_back:
     call _gamma_hc
+    pop rdx
+    pop rcx
+    pop rbx
     ret
