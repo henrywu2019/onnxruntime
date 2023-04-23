40550edd15ed5c7ba62d74dcd48587523acf05eb
Fuheng Wu <fuheng.wu@oracle.com>
2023-04-23 17:12:09 +0000

    arm

--- tutorial/paper/no-fma/run_release.arm.sh
+++ tutorial/paper/no-fma/run_release.arm.sh
@@ -4,6 +4,6 @@ if grep -q avx2 /proc/cpuinfo; then
     INSTRUCTION_FLAG="-mavx2"
 fi
 
-g++ smm_conv.cpp common.cpp -O3 -o smm_conv_release.exe -lpthread -std=c++11 $INSTRUCTION_FLAG -march=native
+g++ smm_conv.cpp common.cpp -O3 -o smm_conv_release.exe -std=c++11 $INSTRUCTION_FLAG -march=armv8-a -mfpu=neon-fp-armv8
 
-g++ zpzr_conv.cpp common.cpp -O3 -o zpzr_conv_release.exe -lpthread -std=c++11 $INSTRUCTION_FLAG -march=native
+g++ zpzr_conv.cpp common.cpp -O3 -o zpzr_conv_release.exe -std=c++11 $INSTRUCTION_FLAG -march=armv8-a -mfpu=neon-fp-armv8
